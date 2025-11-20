93a9654737a373c9b2f7a0e87030388bcf195985
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-11-19 18:06:04 -0700

    Initial commit Daily Summary

---
+++ lib/ui/components/daily_summary_card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DailySummaryCard extends StatelessWidget {
+  const DailySummaryCard({super.key});
+
+  final int completedTasks;
+  final int totalTasks;
+  final String date;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
