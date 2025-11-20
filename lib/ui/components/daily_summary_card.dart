bfa2ffb8c8d6f9c60dedfc3ea93372897fb06856
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-11-19 18:49:44 -0700

    Daily Summary Card InProgress

--- lib/ui/components/daily_summary_card.dart
+++ lib/ui/components/daily_summary_card.dart
@@ -20,7 +20,53 @@ class DailySummaryCard extends StatelessWidget {
 
     return Card(
       shadowColor: colorScheme.shadow.withValues(alpha: 0.2),
-      shape: ContinuousRectangleBorder(),
+      shape: RoundedSuperellipseBorder(
+        borderRadius: BorderRadius.circular(24.0),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(24.0),
+          gradient: LinearGradient(
+            colors: [colorScheme.primary, colorScheme.secondary],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Daily Wins",
+                    style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      color: colorScheme.inversePrimary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                    decoration: BoxDecoration(
+                      color: colorScheme.inversePrimary.withValues(alpha: 0.1),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      date,
+                      style: TextStyle(
+                        color: colorScheme.inversePrimary,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        // child: ,
+      ),
     );
   }
 }
