713e30b3d2d29177cb5429e1d217416bf579c5e9
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-11-28 22:00:59 -0700

    database provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/data/database/database.dart';
+import 'package:habit_tracker/data/providers/database_provider.dart';
 import 'package:habit_tracker/ui/main_page.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  final database = AppDatabase();
+  runApp(
+    ProviderScope(
+      overrides: [databaseProvider.overrideWithValue(database)],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
