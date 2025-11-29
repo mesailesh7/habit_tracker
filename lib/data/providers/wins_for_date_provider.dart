22f94312639b3a8422198b41b1920e9923e11eae
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-11-28 22:43:46 -0700

    win date provider

--- lib/data/providers/wins_for_date_provider.dart
+++ lib/data/providers/wins_for_date_provider.dart
@@ -3,7 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'database_provider.dart';
 
-final habitForDateProvider =
+final winForDateProvider =
     StreamProvider.family<List<WinWithCompletion>, DateTime>((ref, date) {
       final database = ref.watch(databaseProvider);
       return database.watchWinsForDate(date);
